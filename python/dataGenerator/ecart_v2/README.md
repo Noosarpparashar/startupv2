261c77037f2beb0911a25c5cdc731898c04d7637
prasoon <parasharprasoon.947@gmail.com>
2023-06-29 19:46:36 +0530

    ecart code and kafka kraft infra

---
+++ python/dataGenerator/ecart_v2/README.md
@@ -0,0 +1,133 @@
+```markdown
+# E-Cart Data Generator
+
+This repository contains a Python script for generating data for an e-commerce application. The data generator can be Dockerized and run using Docker Compose.
+
+## Prerequisites
+
+- Docker
+- Docker Compose
+
+## Getting Started
+
+```
+1. Clone this repository:
+   ```bash
+   git clone https://github.com/Noosarpparashar/startupv2.git
+   ```
+
+2. Change into the cloned directory:
+   ```bash
+   cd startupv2/python/dataGenerator/ecart
+   
+
+3. Run the following command to start the application using Docker Compose:
+   ```bash
+   docker-compose up -d
+   ```
+
+   This command will start  the PostgreSQL database.
+   You can use pgadmin or dbeaver to connect to the PostgreSQL database by providing the following details:
+```bash
+    Host: localhost
+    Port: 5433
+    Database: postgres
+    Password: 9473
+ ```
+    
+    
+
+
+4. Once the services are up and running, create the necessary database tables by executing the DDL scripts provided in the previous folder. These scripts should start with "db" and include the schema creation.
+   - Create a database named `PINNACLEDB`. 
+   - ```bash
+        create DATABASE PINNACLEDB
+   - Create a schema named `ecart` within the `PINNACLEDB` database.
+   - ```bash
+        create SCHEMA ECART
+5.   Once the services are up and running, database and schema is created, create tables using following script the following DDL script:
+Note:- Make sure you acre creating inside pinnacledb only
+
+```bash
+create table ECART.CUSTOMER (
+  CUSTID VARCHAR(50),
+  CUSTNAME VARCHAR(100),
+  CUSTADD VARCHAR(400)
+); 
+create table ECART.PRODUCTINFO (
+  PRODUCTID INTEGER,
+  PRODUCTNAME VARCHAR(150),
+  PRODCAT VARCHAR(400),
+  STOREID varchar(70)
+);
+
+create table ECART.STOREINFO (
+  STOREID varchar(70),
+  STORENAME VARCHAR(150),
+  STOREADD VARCHAR(400)
+);
+create table ECART.FACT_ORDER (
+  ORDERID SERIAL PRIMARY key,
+  CUSTID VARCHAR(50),
+  PRODUCTID INTEGER,
+  PURCHASETIMESTAMP TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+``` 
+
+6. Dockerize the Python code by building the Docker image. Run the following command:
+   ```bash
+   docker build -t my-ecart-data-generator .
+   ```
+
+7. Create a Docker network to allow communication between containers:
+   ```bash
+   docker network create my-network
+   ```
+
+8. Connect the PostgreSQL container to the Docker network. Replace `my-postgres` with the name of your PostgreSQL Docker container:
+   ```bash
+   docker network connect my-network my-postgres
+   ```
+
+9. Run the Docker container containing the data generator:
+   ```bash
+   docker run --network=my-network my-ecart-data-generator
+   ```
+
+   Note: Inside code we have given port as 5432 but to connect database on pgadmin or dbeaver use port as 5433 because in docker its running on 5432 but it maps to 5433 on localhost. Don't do anything if you wish to go by default
+
+10. [**Optional**] The data generator will start generating data for the e-commerce application based on the defined logic. And if you wish to increase the rate of data generation go to dataGenerator.py and increase [speed](https://github.com/Noosarpparashar/startupv2/blame/master/python/dataGenerator/ecart/dataGenerator.py#:~:text=speed,150)  and rerun following commands
+```bash
+ docker build -t my-ecart-data-generator
+ docker run --network=my-network my-ecart-data-generator
+```
+11. Connect pgadmin and run following commands to check if data is coming
+
+Note:- Make sure your database is pinnacledb only
+```bash
+    Host: localhost
+    Port: 5433
+    Database: pinnacledb
+    Password: 9473
+ ```
+```bash
+   select * from ECART.CUSTOMER
+   select * from ECART.PRODUCTINFO
+   select * from ECART.FACT_ORDER
+   select * from ECART.STOREINFO
+
+```
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+- [Faker](https://faker.readthedocs.io) - Python library for generating fake data.
+- [Psycopg](https://www.psycopg.org) - PostgreSQL adapter for Python.
+
+Feel free to modify the instructions based on your specific requirements or provide additional information as needed.
+```
+```
---
+++ python/dataGenerator/ecart_v2/dataGenerator.py
@@ -0,0 +1,134 @@
+import psycopg2
+import threading
+import time
+import random
+import hashlib
+from faker import Faker
+
+speed = 150
+conn = psycopg2.connect(
+    database="pinnacledb",
+    user="postgres",
+    host="34.28.196.221",
+    port="5432",
+    password="9473"
+)
+fake = Faker()
+
+def generateCustomer(speed=speed):
+    choices = [1, 2, 3]
+    n = random.choice(choices)
+    print("Random value taken to generate customer is", n, "It will sleep for", (600 * n) / speed, "Speed is ", speed)
+    time.sleep((600 * n) / speed)
+
+    custID = 'C' + str(int(time.time()))[-8:]
+    custName = fake.name()
+    custAdd = fake.address()
+
+    cursor = conn.cursor()
+    postgres_insert_query = """INSERT INTO ECART.CUSTOMER (CUSTID, CUSTNAME, CUSTADD) VALUES (%s,%s,%s)"""
+    record_to_insert = (custID, custName, custAdd)
+    cursor.execute(postgres_insert_query, record_to_insert)
+    conn.commit()
+
+    count = cursor.rowcount
+    print(count, "Record inserted successfully into customer table")
+
+    return
+
+def small_hash(input_string):
+    # Use SHA-256 hash function
+    hash_object = hashlib.sha256(input_string.encode())
+    hex_dig = hash_object.hexdigest()
+    # Take a subset of the hash (first 8 characters)
+    return hex_dig[:10]
+
+def generateProductsAndStores():
+    choices = [2.5, 3.5, 4.5]
+    n = random.choice(choices)
+    print("Random value taken to generate products and stores is", n, "It will sleep for", (300 * n) / speed)
+    time.sleep((300 * n) / speed)
+
+    adjectives = ["Compact", "Portable", "Economical", "High-Power", "Durable", "Advanced", "Professional",
+                  "Industrial", "Lightweight", "Heavy-Duty", "Superior", "Quality", "Easy-to-Use", "Premium",
+                  "Innovative", "Reliable", "Sleek", "Multi-Functional", "Wireless", "Smart", "Fast", "Efficient",
+                  "High-Capacity", "Ultra", "Precision", "Affordable", "Versatile", "Luxurious", "Comfortable",
+                  "Rugged"]
+    nouns = ["Drill", "Printer", "Mower", "Oven", "Headphones", "Camera", "Laptop", "Monitor", "Keyboard", "Watch",
+             "Speaker", "Router", "Mouse", "Charger", "Smartphone", "Bag", "Tablet", "Adapter", "Microphone",
+             "Projector", "Scanner", "Controller", "Flashlight", "Blender", "Coffee-Maker", "Fan", "Air-Purifier",
+             "Toolset", "Hair-Dryer", "Tripod"]
+    prefixes = ["Tech", "Digital", "Gadget", "Innovative", "Smart", "Global", "Worldwide", "Pro", "Quality", "Future",
+                "Creative", "Eco", "Express", "Quick", "Mega", "Ultra", "Super", "Hyper", "Fantastic", "Amazing"]
+    infixes = ["&", "and", "N'", "or", "plus", "versus"]
+    suffixes = ["Store", "Hub", "Depot", "Market", "Place", "Plaza", "Mall", "Center", "Outlet", "World", "Galaxy",
+                "Universe", "Domain", "Spot", "Point", "Zone", "Area", "Quarter", "Field", "Territory"]
+
+    adjective = random.choice(adjectives)
+    noun = random.choice(nouns)
+    prefix = random.choice(prefixes)
+    infix = random.choice(infixes)
+    suffix = random.choice(suffixes)
+    storename = prefix + " " + infix + " " + suffix
+
+    productID = int(str(int(time.time()))[4:9])
+    productName = adjective + " " + noun
+    productCategory = noun
+    storeid = small_hash(storename)
+    storeadd = fake.address()
+
+    cursor = conn.cursor()
+    postgres_insert_query_product_info = """INSERT INTO ECART.PRODUCTINFO (PRODUCTID, PRODUCTNAME, PRODCAT, STOREID) VALUES (%s,%s,%s,%s)"""
+    record_to_insert_product_info = (productID, productName, productCategory, storeid)
+    postgres_insert_query_store_info = """INSERT INTO ECART.STOREINFO (STOREID, STORENAME, STOREADD) VALUES (%s,%s,%s)"""
+    record_to_insert_store_info = (storeid, storename, storeadd)
+
+    cursor.execute(postgres_insert_query_product_info, record_to_insert_product_info)
+    cursor.execute(postgres_insert_query_store_info, record_to_insert_store_info)
+    conn.commit()
+
+    count = cursor.rowcount
+    print(count, "Record inserted successfully into product and store table each")
+
+    return
+
+def generateOrderFact():
+    choices = [0.5, 1.5, 1]
+    n = random.choice(choices)
+    print("Random value taken is", n, "It will sleep for", (100 * n / speed), "seconds")
+    time.sleep(100 * n / speed)
+
+    postgreSQL_select_Query_cust = "SELECT CUSTID FROM ECART.CUSTOMER ORDER BY RANDOM() LIMIT 1"
+    postgreSQL_select_Query_product = "SELECT PRODUCTID FROM ECART.PRODUCTINFO ORDER BY RANDOM() LIMIT 1"
+
+    cursor = conn.cursor()
+    cursor.execute(postgreSQL_select_Query_cust)
+    custidrow = cursor.fetchall()
+
+    cursor.execute(postgreSQL_select_Query_product)
+    productidrow = cursor.fetchall()
+    try:
+        custID = custidrow[0][0]
+        productid = productidrow[0][0]
+
+        postgres_insert_query = """INSERT INTO ECART.FACT_ORDER (CUSTID, PRODUCTID) VALUES (%s,%s)"""
+        record_to_insert = (custID, productid)
+
+        cursor.execute(postgres_insert_query, record_to_insert)
+        conn.commit()
+
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into fact table")
+    except Exception as e:
+        # Code to handle other exceptions
+        print("Looks like no data is present in customer or product table for now", str(e))
+    finally:
+        # Code that will be executed regardless of whether an exception occurred
+        print("")
+
+    return
+
+# generateCustomer(speed)
+# generateProductsAndStores()
+# generateOrderFact()
+#conn.close()
